b7efee75fe9d2c59e0b7be249e7bef5eb7c577dd
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2021-05-03 21:05:41 +0200

    Replace english sentences with translated strings

--- telegram_payment_bot/commands.py
+++ telegram_payment_bot/commands.py
@@ -28,7 +28,7 @@ from telegram_payment_bot.config import ConfigTypes
 from telegram_payment_bot.members_kicker import MembersKicker
 from telegram_payment_bot.payments_checker import PaymentsChecker
 from telegram_payment_bot.special_users_list import AuthorizedUsersList
-from telegram_payment_bot.user_helper import UserHelper
+from telegram_payment_bot.helpers import ChatHelper, UserHelper
 from telegram_payment_bot.payments_emailer import PaymentsEmailer
 from telegram_payment_bot.username_checker import UsernameChecker
 
@@ -42,8 +42,7 @@ def GroupChatOnly(exec_cmd_fct: Callable[[Type[CommandBase]], None]) -> Callable
     def decorated(self):
         # Check if private chat
         if self._IsChatPrivate():
-            self._SendMessage("**ERROR**\nThis command can be executed only in the chat group.")
-            return
+            self._SendMessage(self.translator.GetSentence("GROUP_ONLY_ERR"))
         exec_cmd_fct(self)
 
     return decorated
@@ -59,30 +58,7 @@ def GroupChatOnly(exec_cmd_fct: Callable[[Type[CommandBase]], None]) -> Callable
 class HelpCmd(CommandBase):
     # Execute command
     def _ExecuteCommand(self) -> None:
-        # Build message
-        msg = "**HELP**\n"
-        msg += "Hi %s,\n" % UserHelper.GetName(self.cmd_data.User())
-        msg += "welcome to the Telegram Payment Bot.\n"
-        msg += "Here is the list of supported commands.\n"
-        msg += "\n**Generic**\n"
-        msg += "/help : show this message\n"
-        msg += "/alive : show if bot is active\n"
-        msg += "/auth_users : show the list of users authorized to use the bot\n"
-        msg += "/chat_info : show the chat information - **Only group**\n"
-        msg += "/users_list : show the list of all users in group - **Only group**\n"
-        msg += "/invite_link : generate a new invite link - **Only group**\n"
-        msg += "\n**Username Check**\n"
-        msg += "/check_no_username [HOURS_LEFT] : show users with no username - **Only group**\n"
-        msg += "/remove_no_username : remove users with no username - **Only group**\n"
-        msg += "\n**Payments Check**\n"
-        msg += "/email_no_payment [DAYS_LEFT]: send a reminder email to users whose payments is about to expire\n"
-        msg += "/check_no_payment [DAYS_LEFT] [LAST_DAY]: show users whose payments is about to expire - **Only group**\n"
-        msg += "/remove_no_payment : remove users whose payment is expired - **Only group**\n"
-        msg += "\nCommands that can be executed only in chat group are specified in bold text.\n"
-        msg += "\n**NOTE:** it's possible to add \"quiet\" o \"q\" as last parameter to get the result in a private chat, to avoid flooding the chat group."
-
-        # Send message
-        self._SendMessage(msg)
+        self._SendMessage(self.translator.GetSentence("HELP_CMD") % UserHelper.GetName(self.cmd_data.User()))
 
 
 #
@@ -91,7 +67,7 @@ class HelpCmd(CommandBase):
 class AliveCmd(CommandBase):
     # Execute command
     def _ExecuteCommand(self) -> None:
-        self._SendMessage("**STATUS**\nThe bot is alive.")
+        self._SendMessage(self.translator.GetSentence("ALIVE_CMD"))
 
 
 #
@@ -100,11 +76,7 @@ class AliveCmd(CommandBase):
 class AuthUsersCmd(CommandBase):
     # Execute command
     def _ExecuteCommand(self) -> None:
-        # Build message
-        msg = "**AUTHORIZED USERS**\n"
-        msg += "Users authorized to use the bot:\n%s\n" % AuthorizedUsersList(self.config).ToString()
-        # Send message
-        self._SendMessage(msg)
+        self._SendMessage(self.translator.GetSentence("AUTH_USERS_CMD") % AuthorizedUsersList(self.config).ToString())
 
 
 #
@@ -114,13 +86,10 @@ class ChatInfoCmd(CommandBase):
     # Execute command
     @GroupChatOnly
     def _ExecuteCommand(self) -> None:
-        # Build message
-        msg = "**CHAT INFORMATION**\n"
-        msg += "Name: %s\n" % (self.cmd_data.Chat().title if self.cmd_data.Chat().title is not None else "not specified")
-        msg += "Type: %s\n" % self.cmd_data.Chat().type
-        msg += "ID: %d" % self.cmd_data.Chat().id
-        # Send message
-        self._SendMessage(msg)
+        self._SendMessage(self.translator.GetSentence("CHAT_INFO_CMD") %
+                          (ChatHelper.GetTitle(self.cmd_data.Chat()),
+                          self.cmd_data.Chat().type,
+                          self.cmd_data.Chat().id))
 
 
 #
@@ -132,12 +101,9 @@ class UsersListCmd(CommandBase):
     def _ExecuteCommand(self) -> None:
         # Get chat members
         chat_members = ChatMembersGetter(self.client, self.config).GetAll(self.cmd_data.Chat())
-        # Build message
-        msg = "**USERS LIST**\n"
-        msg += "Chat group: **%s**\n" % self.cmd_data.Chat().title
-        msg += "Users list:\n%s" % chat_members.ToString()
         # Send message
-        self._SendMessage(msg)
+        self._SendMessage(self.translator.GetSentence("USERS_LIST_CMD") %
+                          (ChatHelper.GetTitle(self.cmd_data.Chat()), chat_members.ToString()))
 
 
 #
@@ -165,31 +131,30 @@ class CheckNoUsernameCmd(CommandBase):
             # Get parameter
             left_hours = self.cmd_data.Params().GetAsInt(0)
 
-            msg = "**USERNAME CHECK**\n"
-            msg += "Chat group: **%s**\n" % self.cmd_data.Chat().title
-            msg += "Members with no username: **%d**\n" % chat_members.Count()
-            msg += "Members list:\n%s\n" % chat_members.ToString()
-            msg += "Please set the username %s or you'll be removed from the group.\n" % self.__HoursToStr(left_hours)
-            msg += "To set it: **Settings -> Edit Profile -> username**.\n\n"
+            msg = (self.translator.GetSentence("CHECK_USERNAME_P1_CMD") %
+                   (ChatHelper.GetTitle(self.cmd_data.Chat()),
+                    chat_members.Count(),
+                    chat_members.ToString(),
+                    self.__HoursToStr(left_hours)))
 
             support_email = self.config.GetValue(ConfigTypes.SUPPORT_EMAIL)
             if support_email != "":
-                msg += "**The username shall be sent via email to: ** %s." % support_email
+                msg += self.translator.GetSentence("CHECK_USERNAME_P2_CMD") % support_email
         else:
-            msg = "**USERNAME CHECK**\nChat group: **%s**\nAll members has the username set." % self.cmd_data.Chat().title
+            msg = self.translator.GetSentence("CHECK_USERNAME_P3_CMD") % ChatHelper.GetTitle(self.cmd_data.Chat())
 
         # Send message
         self._SendMessage(msg)
 
     # Get time string
-    @staticmethod
-    def __HoursToStr(hours: int) -> str:
+    def __HoursToStr(self,
+                     hours: int) -> str:
         if hours > 47:
-            return "within %d days" % (hours // 24)
+            return self.translator.GetSentence("WITHIN_DAYS_MSG") % (hours // 24)
         elif hours > 1:
-            return "within %d hours" % hours
+            return self.translator.GetSentence("WITHIN_HOURS_MSG") % hours
         else:
-            return "as soon as possible"
+            return self.translator.GetSentence("ASAP_MSG")
 
 
 #
@@ -200,16 +165,18 @@ class RemoveNoUsernameCmd(CommandBase):
     @GroupChatOnly
     def _ExecuteCommand(self) -> None:
         # Notice before removing
-        self._SendMessage(
-            "**USERNAME CHECK**\nChat group: **%s**\nThe members without username are going to be removed." % self.cmd_data.Chat().title)
+        self._SendMessage(self.translator.GetSentence("REMOVE_NO_USERNAME_P1_CMD") %
+                          ChatHelper.GetTitle(self.cmd_data.Chat()))
 
         # Kick members if any
-        kicked_members = MembersKicker(self.client, self.config, self.logger).KickAllWithNoUsername(self.cmd_data.Chat())
+        kicked_members = MembersKicker(self.client,
+                                       self.config,
+                                       self.logger).KickAllWithNoUsername(self.cmd_data.Chat())
 
         # Build message
-        msg = "Operation completed. Removed members: **%d**" % kicked_members.Count()
+        msg = self.translator.GetSentence("REMOVE_NO_USERNAME_P2_CMD") % kicked_members.Count()
         if kicked_members.Any():
-            msg += "\nMembers list:\n%s" % kicked_members.ToString()
+            msg += self.translator.GetSentence("REMOVE_NO_USERNAME_P3_CMD") % kicked_members.ToString()
 
         # Send message
         self._SendMessage(msg)
@@ -225,24 +192,28 @@ class EmailNoPaymentCmd(CommandBase):
     # Execute command
     def _ExecuteCommand(self) -> None:
         if not self.config.GetValue(ConfigTypes.EMAIL_ENABLED):
-            msg = "**PAYMENTS CHECK**\nEmail is currently disabled."
+            msg = self.translator.GetSentence("EMAIL_NO_PAYMENT_DISABLED_CMD")
         else:
             # Get parameter
             days_left = self.cmd_data.Params().GetAsInt(0)
             # Get expired payments
-            expired_payments = PaymentsEmailer(self.client, self.config, self.logger).EmailAllWithExpiringPayment(days_left)
+            expired_payments = PaymentsEmailer(self.client,
+                                               self.config,
+                                               self.logger).EmailAllWithExpiringPayment(days_left)
 
             # Build message
             if expired_payments.Any():
                 # Build strings for easier reading
-                days_left_str = "in %d days" % days_left if days_left > 1 else ("tomorrow" if days_left == 1 else "today")
+                days_left_str = (self.translator.GetSentence("IN_DAYS_MSG") % days_left
+                                 if days_left > 1 else
+                                 (self.translator.GetSentence("TOMORROW_MSG")
+                                  if days_left == 1 else
+                                  self.translator.GetSentence("TODAY_MSG")))
 
-                msg = "**PAYMENTS CHECK**\n"
-                msg += "Members whose payment is about to expire %s: **%d**\n" % (days_left_str, expired_payments.Count())
-                msg += "Members list:\n%s\n" % expired_payments.ToString()
-                msg += "A reminder email has been sent to all members."
+                msg = (self.translator.GetSentence("EMAIL_NO_PAYMENT_P1_CMD") %
+                       (days_left_str, expired_payments.Count(), expired_payments.ToString()))
             else:
-                msg = "**PAYMENTS CHECK**\nAll members paid, no email sent."
+                msg = self.translator.GetSentence("EMAIL_NO_PAYMENT_P2_CMD")
 
         # Send message
         self._SendMessage(msg)
@@ -259,36 +230,46 @@ class CheckNoPaymentCmd(CommandBase):
         days_left = self.cmd_data.Params().GetAsInt(0)
         last_day = self.cmd_data.Params().GetAsInt(1)
         # Get expired members
-        expired_members = PaymentsChecker(self.client, self.config, self.logger).GetAllMembersWithExpiringPayment(self.cmd_data.Chat(), days_left)
+        expired_members = PaymentsChecker(self.client,
+                                          self.config,
+                                          self.logger).GetAllMembersWithExpiringPayment(self.cmd_data.Chat(), days_left)
 
         # Build message
         if expired_members.Any():
             # Build strings for easier reading
-            days_left_str = "in %d days" % days_left if days_left > 1 else ("tomorrow" if days_left == 1 else "today")
-            last_day_str = "day %d of the month" % last_day if 1 <= last_day <= 31 else "few days"
+            days_left_str = (self.translator.GetSentence("IN_DAYS_MSG") % days_left
+                             if days_left > 1 else
+                             (self.translator.GetSentence("TOMORROW_MSG")
+                              if days_left == 1 else
+                              self.translator.GetSentence("TODAY_MSG")))
+            last_day_str = (self.translator.GetSentence("DAY_OF_MONTH_MSG") % last_day
+                            if 1 <= last_day <= 31
+                            else self.translator.GetSentence("FEW_DAYS_MSG"))
 
-            msg = "**PAYMENTS CHECK**\n"
-            msg += "Chat group: **%s**\n" % self.cmd_data.Chat().title
-            msg += "Members whose payment is about to expire %s: **%d**\n" % (days_left_str, expired_members.Count())
-            msg += "Members list:\n%s\n" % expired_members.ToString()
-            msg += "Please pay within %s or you'll be removed from the group.\n" % last_day_str
+            # Build message
+            msg = (self.translator.GetSentence("CHECK_NO_PAYMENT_P1_CMD") %
+                   (ChatHelper.GetTitle(self.cmd_data.Chat()),
+                    days_left_str,
+                    expired_members.Count(),
+                    expired_members.ToString(),
+                    last_day_str))
 
+            # Add website if any
             website = self.config.GetValue(ConfigTypes.PAYMENT_WEBSITE)
             if website != "":
-                msg += "You can pay directly on the website: %s\n\n" % website
+                msg += self.translator.GetSentence("CHECK_NO_PAYMENT_P2_CMD") % website
 
+            # Add contact information if any
             support_email = self.config.GetValue(ConfigTypes.SUPPORT_EMAIL)
             support_tg = self.config.GetValue(ConfigTypes.SUPPORT_TELEGRAM)
-
             if support_email != "" and support_tg != "":
-                msg += "If you are on the list but you paid, please send an email to %s or contact @%s **specifying your Telegram username**." % (
-                    support_email, support_tg)
+                msg += self.translator.GetSentence("CHECK_NO_PAYMENT_P3_CMD") % (support_email, support_tg)
             elif support_email != "":
-                msg += "If you are on the list but you paid, please send an email to %s **specifying your Telegram username**." % support_email
+                msg += self.translator.GetSentence("CHECK_NO_PAYMENT_P4_CMD") % support_email
             elif support_tg != "":
-                msg += "If you are on the list but you paid, please contact @%s **specifying your Telegram username**." % support_tg
+                msg += self.translator.GetSentence("CHECK_NO_PAYMENT_P5_CMD") % support_tg
         else:
-            msg = "**PAYMENTS CHECK**\nChat group: **%s**\nAll members paid." % self.cmd_data.Chat().title
+            msg = self.translator.GetSentence("CHECK_NO_PAYMENT_P6_CMD") % ChatHelper.GetTitle(self.cmd_data.Chat())
 
         # Send message
         self._SendMessage(msg)
@@ -302,15 +283,18 @@ class RemoveNoPaymentCmd(CommandBase):
     @GroupChatOnly
     def _ExecuteCommand(self) -> None:
         # Notice before removing
-        self._SendMessage("**PAYMENTS CHECK**\nChat group: **%s**\nMembers whose payment is about to expire are going to be removed." % self.cmd_data.Chat().title)
+        self._SendMessage(self.translator.GetSentence("REMOVE_NO_PAYMENT_P1_CMD") %
+                          ChatHelper.GetTitle(self.cmd_data.Chat()))
 
         # Kick members if any
-        kicked_members = MembersKicker(self.client, self.config, self.logger).KickAllWithExpiredPayment(self.cmd_data.Chat())
+        kicked_members = MembersKicker(self.client,
+                                       self.config,
+                                       self.logger).KickAllWithExpiredPayment(self.cmd_data.Chat())
 
         # Build message
-        msg = "Operation completed. Removed members: **%d**" % kicked_members.Count()
+        msg = self.translator.GetSentence("REMOVE_NO_PAYMENT_P2_CMD") % kicked_members.Count()
         if kicked_members.Any():
-            msg += "\nMembers list:\n%s" % kicked_members.ToString()
+            msg += self.translator.GetSentence("REMOVE_NO_PAYMENT_P3_CMD") % kicked_members.ToString()
 
         # Send message
         self._SendMessage(msg)
