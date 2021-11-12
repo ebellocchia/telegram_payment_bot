e40caa9a0e34617150e8749f37e4b32ae0ec31f7
Emanuele Bellocchia <54482000+ebellocchia@users.noreply.github.com>
2021-11-12 12:45:42 +0100

    Fix type

--- telegram_payment_bot/message_sender.py
+++ telegram_payment_bot/message_sender.py
@@ -22,7 +22,7 @@
 # Imports
 #
 import time
-from typing import List, Union
+from typing import Any, List, Union
 import pyrogram
 from telegram_payment_bot.logger import Logger
 
@@ -56,7 +56,7 @@ class MessageSender:
     def SendMessage(self,
                     receiver: Union[pyrogram.types.Chat, pyrogram.types.User],
                     msg: str,
-                    **kwargs) -> List[pyrogram.types.Message]:
+                    **kwargs: Any) -> List[pyrogram.types.Message]:
         # Log
         self.logger.GetLogger().info(f"Sending message (length: {len(msg)}):\n{msg}")
         # Split and send message
@@ -66,7 +66,7 @@ class MessageSender:
     def __SendSplitMessage(self,
                            receiver: Union[pyrogram.types.Chat, pyrogram.types.User],
                            split_msg: List[str],
-                           **kwargs) -> List[pyrogram.types.Message]:
+                           **kwargs: Any) -> List[pyrogram.types.Message]:
         sent_msgs = []
 
         # Send message
